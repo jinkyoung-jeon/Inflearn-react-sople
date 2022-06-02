829d3396f07c4e85d57c9b1b853082b1eec1df16
JeonJinKyoung <kyeong1750@naver.com>
2022-06-02 18:18:42 +0900

    (실습) 댓글 컴포넌트 만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import Clock from './chapter04/Clock';
+import CommentList from './chapter05/CommentList';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './chapter03/Library';
+// import Library from './chapter03/Library';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+setInterval(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <CommentList />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
