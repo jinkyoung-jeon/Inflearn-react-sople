ade4f9f5712ad800233d5f7af2d1a4568ad2f7a2
JeonJinKyoung <kyeong1750@naver.com>
2022-06-03 18:31:05 +0900

    여러 개의 Component 렌더링 하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 // import Clock from './chapter04/Clock';
 import CommentList from './chapter05/CommentList';
+import NotificationList from './chapter06/NotificationList';
+import Accommodate from './chapter07/Accommodate';
+import ConfirmButton from './chapter08/ConfirmButton';
+import LandingPage from './chapter09/LandingPage';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 // import Library from './chapter03/Library';
 
-setInterval(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <CommentList />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}, 1000);
+// setInterval(() => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <Accommodate />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// }, 1000);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<LandingPage />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
